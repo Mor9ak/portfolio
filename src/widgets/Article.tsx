81a6776920d39566e8c00b362c9a5ec279242b78
Mor9ak <skricheery0rik@gmail.com>
2026-07-22 14:06:52 +0300

    style(article): - change max width mobile/pc - add text-wrap

--- src/widgets/Article.tsx
+++ src/widgets/Article.tsx
@@ -13,9 +13,9 @@ interface ArtInfo {
 
 const Article = (props: ArtInfo) => {
     return (
-        <article className={"[&>*]:text-textCol bg-bgArticle flex flex-col w-fit m-5 p-2 rounded-lg *:m-3 self-start max-w-md"}>
+        <article className={"[&>*]:text-textCol bg-bgArticle flex flex-col w-fit max-w-full md:max-w-md m-5 p-2 rounded-lg *:m-3 self-start "}>
             <h2 className={"text-2xl flex items-center justify-center"}>{props.title}</h2>
-            <p className={"flex items-center justify-center px-6"}>{props.text}</p>
+            <p className={"flex items-center justify-center px-6 text-wrap"}>{props.text}</p>
             <div id={"buttonSec"} className={"flex flex-wrap items-center justify-around *:rounded-lg *:text-textCol"}>
                 {props.buttons.map((button: ButtonProps) => (
                     <Button key={button.buttonText} title={button.buttonText} link={button.ButtonUrl} />
